9404a0551d7d1269e4864f2b27d00556f264a5fa
komunizmabuilder <84953478+komunizmabuilder@users.noreply.github.com>
2021-05-28 12:36:53 +0700

    h
    h

---
+++ New folder/New Text Document.bat
@@ -0,0 +1,56 @@
+@echo off
+:a
+color 2
+echo                                     `-/+oyhddddmmmmmmddddhso+:.`                                    
+echo                                -+shmdhso/:-.```      ```.-:/oyhdmhs/-                               
+echo                           `:ohmhs/-`                            `-+shmho:`                          
+echo                        .+hmho-`                                      `:ohmh+.                       
+echo                     .+dds:`                                              `/ymd+.                    
+echo                  `/hmy:`                                                    `/ymy:                  
+echo                `+dd+.                                                          .odd+`               
+echo              `+mh/`                                                              `/dm+`             
+echo            `/dd/`                                                                  `/dd/            
+echo           -hm+`        .:-             ``...------------...``             --.        `omy-          
+echo         `+my.      `:sdh:`         `.....--.``-.`-.`.-``.--.....`         `:hds:`      .hm+         
+echo        `ym+`   ..`omMNo`       `....```..`  `-.  -.  .-   `..```...`        `sNMd+`.`   `+Ny`       
+echo       -dd-   :yo-hMMm/       `.-.`   .-`    -.   -.   -.    `-.   `--.`       /mMMh-oy:   :md.      
+echo      -md.  `sNs`dMms:/     `-.``....--`    `-`   -.   `-`    `--....``..`     /:sNMh`yNs   .mm-     
+echo     -mh. ``sMN-+do/sd.   `-.`     .-.......--``-:+oo/-`--.......-.     `.-`   -do/od/-MMo`` .dm.    
+echo    .md. :+-NMh`/+hNd-   .-`      `-`     `.-``+Nh-:hMN+`-.`     `-`      `-.   :mNh+/`dMN.o- -mm`   
+echo   `hN- :N//MN/smNdo`  `-.       `-`       .-  :hs-.sMMo -.       `-`       .-`  .omNmo+NM:+N- :Ny   
+echo   +M+ .mM/:MhmNy/-   `-`        -.        -`     -+mh/` .-        .-        `-`   :/hNdhN-+Mm` oM+  
+echo  .Nh  +MM+.Nms:/h-  `--..``    .-        `-      :y-    `:`        -.    ``..--`  :h/:ymN`sMM/  dm. 
+echo  sM:  sMMy.y:omMo  `-` ``......-.``      .-      ::      -.      ``.-......`` .-`  sMd+:y`dMMo  :Mo 
+echo `Nd  `oMMd`/mMNo   -.         .-`........--...``/dd/``...--........`-.         .-   sMMm/`dMM+` `dm`
+echo /M+ ./.NMyoMMm/   .-          -.        `-.`````/dd:`````.-`        .-          -`   +mMM+dMN./. oM:
+echo yN- ss oMyNMs.+`  -.         `-`         -`      --      .-         `-`         .-  `o.yMmyM+ yo -Ms
+echo dm` hN.`mmN/`sh  `-`         `-          :`  ``-++++.``  `-          :`         `-` `do /Nmd -Ny .Nh
+echo Nd  hMh`:N:`hMo  `:          .-          :`:y.-smMMdo--s:.-          -`         `:`  sMh`/N-`dMy `md
+echo Md  +MMh`/.mMN.  `--......--------..----/smMm`  :NN:  `NMms:--------------...----:`  -NMd./`hMM/ `dm
+echo Nd  .NMMs hMMs   `:          .- :+ooyhmNMMMM/  .:hy/.  +MMMMNdhsoo+: -`         `:`   yMMy yMMm` `md
+echo dm` ./NMN:mMd`+  `-`         `-+MMMMMMMMMMMM`   `md`   .MMMMMMMMMMMM+:`         `-`  /`mMm:MMN/. .Nh
+echo yN- s`:mMsmM:`d:  -.         `-dMMMMMMMMMMMM.   +MM/   -MMMMMMMMMMMMh-`         .-  /d /MmyMm:.s :Ms
+echo /M+ yd..ymNm :Mh  `-          :NMMMMMMMMMMMMs   yMMs   yMMMMMMMMMMMMN-         `-`  dN- Nmmy`-ds oM:
+echo `Nd :MN+`/mh yMM`  -.        `+MMMMMMMMMMMMMN:  yMMs  /MMMMMMMMMMMMMM/         .-  `MMs dm:`oNN-`mm`
+echo  sM: oMMd+-+`mMM.` `-` ``.....yMMMMMMMMMMMMMMN: hMMy /NMMMMMMMMMMMMMMs.....`` .-` `:MMd o-+mMN+ /Mo 
+echo  .Nd  /mMMd/`dMM:-o``--.```  `hMMMMMMMMMMMMMMMN+dMMhoNMMMMMMMMMMMMMMMy   ```.--``o./MMd`/mMMm:  mm` 
+echo   +M+  .yMMNooMM+ dh``-`     -NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.     .-`.dh oMM+sMMNy.  oM/  
+echo   `hN- :::smMhdMh oMd.`-.    :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:    .-`.mM+ dMhdMms-:: :Ny   
+echo    `mm.`ys-./ydNN--MMh` .-`  sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo  `-. .dMN.:NNdy:.:yy -md`   
+echo     -md.`yNh+-.:ys`dMMs`.`-.`hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh`..`.`yMMh`ys:.-+dNs`.md.    
+echo      -md. /dMNdy+/.-mMN:/y:.-mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd-./y:/MMm.-/+ymNMh: -md.     
+echo       .dm: `:yNMMNms+dMm.+Nh/NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/dN/-NMd+ymNMMmy:` :mh.      
+echo        `yN+`  :+ymNNMddNd-+NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+-mNdmMNNmy+-  `oNs`       
+echo          +mh. `+/:::/+sydh:/dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd::hdys+/:::/+` -hm/         
+echo           -ym+` /yhhysooooo+/oymNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmyo/+ooooosyhhy: .omy.          
+echo             /dd/` .+ymNNMMMMMNNmddhsyMMMMMMMMMMMMMMMMMMMMMMMMMMsshddmNMMMMMMNNmy/. `+dd:            
+echo              `+dd/`  `:://+++/:--.:odMMMMMMMMMMMMMMMMMMMMMMMMMMho:.--:/+++//::`  `+dd/`             
+echo                `+ddo. ./sssooosydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdysooosss/. .odd/`               
+echo                   :ymy/``:+ydmmmmmdydMMMMMMMMMMMMMMMMMMMMMMMMMMdydmmmmmdy+-`./hmy:                  
+echo                     .+hmy/`   ```   hMMMMMMMMMMMMMMMMMMMMMMMMMMy   ```  `./ymh/`                    
+echo                        .+hmho:`     mMMMMMMMMMMMMMMMMMMMMMMMMMMh     .:ohmy/.                       
+echo                           `-ohmds+:`MMMMMMMMMMMMMMMMMMMMMMMMMMMN`:+ydmho-`                          
+echo                                ./ohmNMMMMMMMMMMMMMMMMMMMMMMMMMMNmho/.                               
+echo                                     `.:/osyhdmmmNNNNmmmdhyso/:.`                                    
+pause
+start cmd.exe
---
+++ New folder/script.vbs
@@ -0,0 +1,1 @@
+X=MsgBox("cant open internet explorer",0+16,"ERROR!")
